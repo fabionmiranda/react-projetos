af99e96ef8a2a3894801036df2e20c0b14b64891
Fabio N. Miranda <fabionmiranda@gmail.com>
2019-06-07 18:17:58 -0300

    projeto react web

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Header from './componentes/Header';
+import React from 'react';
+import Principal from './paginas/principal';
+
+
+const App = () => 
+  <div className="App">
+        <Header />
+        <Principal />
+  </div>
+
+
+export default App;
---
+++ src/paginas/principal/index.js
@@ -0,0 +1,8 @@
+import React, {Component } from 'react';
+import api from '../../servicos/api';
+
+export default class Principal extends Component {
+    render() {
+        return <h1> ola...</h1>
+    }
+}
